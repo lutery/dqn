034b6b214d3fcfb22ded54ddb8f4f1015f68b42a
luctery <luctery@foxmail.com>
2024-06-25 19:38:56 +0800

    提交CarRacing的测试代码

--- learning/CarRacing/play_a2c_linear.py
+++ learning/CarRacing/play_a2c_linear.py
@@ -11,6 +11,7 @@ from PIL import Image
 
 import numpy as np
 import torch
+import torch.nn as nn
 
 class AtariA2C(nn.Module):
     def __init__(self, input_shape, n_actions):
@@ -50,35 +51,44 @@ class AtariA2C(nn.Module):
         return self.policy(conv_out), self.value(conv_out)
 
 
+class TransposeObservation(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(TransposeObservation, self).__init__(env)
+
+    def observation(self, observation):
+        # 将观察从 (H, W, C) 转换为 (C, H, W)
+        return observation.transpose(2, 0, 1)
+
+
 ENV_ID = "CarRacing-v2"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", required=True, help="Model file to load")
+    parser.add_argument("-m", "--model", default=r"M:\Projects\python\my_-nqd\learning\CarRacing\saves\a2c-ppo_carracing_linear\a2c-best-935.19999999999.pth", help="Model file to load")
     parser.add_argument("-e", "--env", default=ENV_ID, help="Environment name to use, default=" + ENV_ID)
     parser.add_argument("-r", "--record", help="If specified, sets the recording dir, default=Disabled")
     parser.add_argument("-s", "--save", type=int, help="If specified, save every N-th step as an image")
     # parser.add_argument("--acktr", default=False, action='store_true', help="Enable Acktr-specific tweaks")
     args = parser.parse_args()
 
-    env = gym.make(args.env, render_mode="human")
+    env = TransposeObservation(gym.make(args.env, render_mode="human", continuous=False))
     if args.record:
         env = gym.wrappers.Monitor(env, args.record)
 
-    net = model.ModelActor(env.observation_space.shape[0], env.action_space.shape[0])
+    net = AtariA2C(env.observation_space.shape, env.action_space.n)
     # if args.acktr:
     #     opt = kfac.KFACOptimizer(net)
     net.load_state_dict(torch.load(args.model))
+    net.to(torch.device("cuda"))
 
     obs, _ = env.reset()
     total_reward = 0.0
     total_steps = 0
     while True:
-        obs_v = torch.FloatTensor(obs)
-        mu_v = net(obs_v)
+        obs_v = torch.FloatTensor([obs]).to("cuda")
+        mu_v = net(obs_v)[0].to("cpu")
         action = mu_v.squeeze(dim=0).data.numpy()
-        action = np.clip(action, -1, 1)
-        obs, reward, done, trunc, _ = env.step(action)
+        obs, reward, done, trunc, _ = env.step(np.argmax(action))
         done = done or trunc
         total_reward += reward
         total_steps += 1
