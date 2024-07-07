a07773660a2943c2d896b05b176021a69e06dc1e
lutery <lutery@foxmail.com>
2024-07-07 18:11:04 +0800

    提交td3算法

--- learning/pendulum/train_td3.py
+++ learning/pendulum/train_td3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 '''
-验证通过，可以正常的执行训练，并且其损失可以i正常的收敛，奖励会逐渐增加，和书中的一样，要训练比较长的时间步数才行，我这边的测试结果要在230K时，loss_actor才会下降到负区域，然后一直下降，虽然奖励也会出现下降趋势，但是放眼更大的区域，奖励总体是上升的趋势
+未验证
+1. 本游戏
+2. 其他游戏
+3. 补充注释，对比ddpg，提取关键点
 '''
 
 import os
@@ -15,6 +18,7 @@ from lib import model, common
 
 import torch
 import torch.optim as optim
+import torch.distributions as dist
 import torch.nn.functional as F
 
 
@@ -27,8 +31,14 @@ REPLAY_INITIAL = 10000 # 重放缓冲区初始化大小
 
 TEST_ITERS = 1000
 
+eval_noise_scale = 0.5
+explore_noise_scale = 1.0
+reward_scale = 1.
+policy_target_update_interval = 3
+action_range = 1.
+
 class TD3():
-    def __init__(self, obs_space, device="cpu"):
+    def __init__(self, obs_space, action_range, device="cpu"):
         self.device = device
         self.act_net = model.DDPGActor(obs_space.shape[0], obs_space.shape[0]).to(device)
         self.crt1_net = model.DDPGCritic(obs_space.shape[0], obs_space.shape[0]).to(device)
@@ -36,12 +46,13 @@ class TD3():
         self.target_crt1_net = ptan.agent.TargetNet(self.crt1_net)
         self.target_crt2_net = ptan.agent.TargetNet(self.crt2_net)
         self.target_act_net = ptan.agent.TargetNet(self.act_net)
-        self.agent = model.AgentDDPG(self.act_net, device=device)
 
         self.act_opt = optim.Adam(self.act_net.parameters(), lr=LEARNING_RATE)
         self.crt1_opt = optim.Adam(self.crt1_net.parameters(), lr=LEARNING_RATE)
         self.crt2_opt = optim.Adam(self.crt2_net.parameters(), lr=LEARNING_RATE)
 
+        self.action_range = action_range
+        self.device = device
         self.update_cnt = 0
 
 
@@ -49,12 +60,50 @@ class TD3():
         self.update_cnt += 1
 
 
-class TD3Agent(ptan.agent):
-    def __init__(self, net):
-        super().__init__(net)
+    def evaluate(self, states, eval_noise_scale):
+        states_v = ptan.agent.float32_preprocessor(states).to(self.device)
+        mu_v = self.target_act_net.target_model(states_v)
+        actions = mu_v.data.cpu().numpy()
+        actions = self.action_range * actions
+        normal = dist.Normal(0, 1)
+        eval_noise_clip = 2* eval_noise_scale
+        noise = normal.sample(actions.shape) * eval_noise_scale
+        noise = np.clip(noise, -eval_noise_clip, eval_noise_clip)
+        actions = np.add(actions, noise)
+
+        return torch.tensor(actions, dtype=torch.float32).to(self.device)
 
 
-    def evaluate(self, states):
+class TD3Agent(ptan.agent.BaseAgent):
+    def __init__(self, net, explore_noise_scale, action_range=1., step=0, device="cpu"):
+        '''
+
+        :param net:
+        :param explore_noise_scale:
+        :param action_range: todo 这里的动作范围是否需要考虑到不同的游戏
+        '''
+        assert isinstance(net, model.DDPGActor)
+        self.net = net
+        self.explore_noise_scale = explore_noise_scale
+        self.action_range = action_range
+        self.step = step
+        self.device = device
+        self.rng = np.random.default_rng(int(time.time()))
+
+    def __call__(self, states, agent_states):
+        # todo 这边用原来的OU方法是否也可以？
+        states_v = ptan.agent.float32_preprocessor(states).to(self.device)
+        mu_v = self.net(states_v)
+        actions = mu_v.data.cpu().numpy()
+        # todo 500是否可以不用
+        if self.step < 500:
+            actions = self.rng.uniform(-1, 1, actions.shape)
+            actions = self.action_range * actions
+        else:
+            noise = self.rng.normal(0, 1, actions.shape) * self.explore_noise_scale
+            actions = self.action_range * actions + noise
+
+        return actions, agent_states
 
 
 
@@ -97,15 +146,27 @@ if __name__ == "__main__":
     test_env = gym.make(ENV_ID, g=9.81)
 
     # 构建动作网络和评价网络
-    td3 = TD3(env.observation_space, device=device)
+    td3 = TD3(env.observation_space, action_range=action_range, device=device)
     print(td3.act_net)
     print(td3.crt1_net)
     print(td3.crt2_net)
 
-    writer = SummaryWriter(comment="-ddpg_" + args.name)
-    exp_source = ptan.experience.ExperienceSourceFirstLast(env, td3.agent, gamma=GAMMA, steps_count=1)
-    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=REPLAY_SIZE)
+    agent = TD3Agent(td3.act_net, action_range=action_range, explore_noise_scale=explore_noise_scale, device=device)
+    if (os.path.exists(os.path.join(save_path, "checkpoint_0.dat"))):
+        checkpoint = torch.load(os.path.join(save_path, "checkpoint_0.dat"))
+        td3.act_net.load_state_dict(checkpoint["act_net"])
+        td3.crt1_net.load_state_dict(checkpoint["crt1_net"])
+        td3.crt2_net.load_state_dict(checkpoint["crt2_net"])
+        td3.target_act_net.target_model.load_state_dict(checkpoint["target_act_net"])
+        td3.target_crt1_net.target_model.load_state_dict(checkpoint["target_crt1_net"])
+        td3.target_crt2_net.target_model.load_state_dict(checkpoint["target_crt2_net"])
+        td3.update_cnt = checkpoint["update_cnt"]
+        agent.step = 1000
+        print("加载模型成功")
 
+    writer = SummaryWriter(comment="-ddpg_" + args.name)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=GAMMA, steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=REPLAY_SIZE)
 
     frame_idx = 0
     best_reward = None
@@ -126,65 +187,89 @@ if __name__ == "__main__":
                 if len(buffer) < REPLAY_INITIAL:
                     continue
 
+                td3.update()
                 # 从缓冲区里面采样数据
                 batch = buffer.sample(BATCH_SIZE)
                 states_v, actions_v, rewards_v, dones_mask, last_states_v = common.unpack_batch_ddqn(batch, device)
 
-                # train critic
-                crt_opt.zero_grad()
-                # 根据状态和动作，得到评价，这里是根据实际游戏的状态和动作获取评价
-                q_v = crt_net(states_v, actions_v)
+                # 归一化奖励
+                rewards_v = reward_scale * (rewards_v - rewards_v.mean()) / (rewards_v.std() + 1e-6)
                 # 使用目标动作预测网路，根据下一个状态预测执行的动作
-                last_act_v = tgt_act_net.target_model(last_states_v)
+                last_act_v = td3.evaluate(last_states_v, eval_noise_scale)
                 # 使用目标评测网络，根据下一个状态和下一个状态将要执行的动作得到下一个状态的评价Q值
-                q_last_v = tgt_crt_net.target_model(last_states_v, last_act_v)
+                q_last_v = torch.min(td3.target_crt1_net.target_model(last_states_v, last_act_v), td3.target_crt2_net.target_model(last_states_v, last_act_v))
                 # 如果是结束状态则将奖励置为0
                 q_last_v[dones_mask.bool()] = 0.0
                 # 计算Q值 bellman公式
                 q_ref_v = rewards_v.unsqueeze(dim=-1) + q_last_v * GAMMA
                 # 计算预测的当前Q值和Bellman计算的到的Q值之间的差异
                 # 并更新梯度 这里的方式就和之前的Q值单元的一致
-                critic_loss_v = F.mse_loss(q_v, q_ref_v.detach())
-                critic_loss_v.backward()
-                crt_opt.step()
-                tb_tracker.track("loss_critic", critic_loss_v, frame_idx)
+                # train critic
+                td3.crt1_opt.zero_grad()
+                # 根据状态和动作，得到评价，这里是根据实际游戏的状态和动作获取评价
+                q_v = td3.crt1_net(states_v, actions_v)
+                critic_1_loss_v = F.mse_loss(q_v, q_ref_v.detach())
+                critic_1_loss_v.backward()
+                td3.crt1_opt.step()
+                tb_tracker.track("loss_critic_1", critic_1_loss_v, frame_idx)
+
+                td3.crt2_opt.zero_grad()
+                q_v = td3.crt2_net(states_v, actions_v)
+                critic_2_loss_v = F.mse_loss(q_v, q_ref_v.detach())
+                critic_2_loss_v.backward()
+                td3.crt2_opt.step()
+                tb_tracker.track("loss_critic_2", critic_2_loss_v, frame_idx)
                 tb_tracker.track("critic_ref", q_ref_v.mean(), frame_idx)
 
-                # train actor
-                act_opt.zero_grad()
                 # 预测动作
-                cur_actions_v = act_net(states_v)
-                # 根据状态和预测的动作计算Q值的负值
-                # 这里是根据网络预测的动作和实际的状态获取评价
-                # 在评价前取负号，就是简单粗暴的取最小值，从而达到最大值Q值的目的
-                # 由于这里评价网络是固定是，所以最大化Q值，只有更新预测的动作，使得
-                # 评价Q值达到最大值的目的
-                actor_loss_v = -crt_net(states_v, cur_actions_v)
-                actor_loss_v = actor_loss_v.mean()
-                actor_loss_v.backward()
-                act_opt.step()
-                tb_tracker.track("loss_actor", actor_loss_v, frame_idx)
+                if td3.update_cnt % policy_target_update_interval == 0:
+                    # train actor
+                    td3.act_opt.zero_grad()
+                    cur_actions_v = td3.evaluate(states_v, 0)
+                    # 根据状态和预测的动作计算Q值的负值
+                    # 这里是根据网络预测的动作和实际的状态获取评价
+                    # 在评价前取负号，就是简单粗暴的取最小值，从而达到最大值Q值的目的
+                    # 由于这里评价网络是固定是，所以最大化Q值，只有更新预测的动作，使得
+                    # 评价Q值达到最大值的目的
+                    actor_loss_v = -td3.crt1_net(states_v, cur_actions_v)
+                    actor_loss_v = actor_loss_v.mean()
+                    actor_loss_v.backward()
+                    td3.act_opt.step()
+                    tb_tracker.track("loss_actor", actor_loss_v, frame_idx)
 
-                # 将训练网路同步到目标网络上，但是这里是每次都同步，与之前每隔n步同步一次不同
-                # 这里之所以这样做，是根据测试可知，每次都同步，并使用较小的权重进行同步
-                # 缓存的同步效果更好，并且能够保持平滑的更新
-                tgt_act_net.alpha_sync(alpha=1 - 1e-3)
-                tgt_crt_net.alpha_sync(alpha=1 - 1e-3)
+                    # 将训练网路同步到目标网络上，但是这里是每次都同步，与之前每隔n步同步一次不同
+                    # 这里之所以这样做，是根据测试可知，每次都同步，并使用较小的权重进行同步
+                    # 缓存的同步效果更好，并且能够保持平滑的更新
+                    td3.target_act_net.alpha_sync(alpha=1 - 1e-3)
+                    td3.target_crt1_net.alpha_sync(alpha=1 - 1e-3)
+                    td3.target_crt2_net.alpha_sync(alpha=1 - 1e-3)
 
                 if frame_idx % TEST_ITERS == 0:
                     # 测试并保存最好测试结果的庶数据
                     ts = time.time()
-                    rewards, steps = test_net(act_net, test_env, device=device)
+                    rewards, steps = test_net(td3.act_net, test_env, device=device)
                     print("Test done in %.2f sec, reward %.3f, steps %d" % (
                         time.time() - ts, rewards, steps))
                     writer.add_scalar("test_reward", rewards, frame_idx)
                     writer.add_scalar("test_steps", steps, frame_idx)
+                    checkpoint = {
+                        "act_net": td3.act_net.state_dict(),
+                        "crt1_net": td3.crt1_net.state_dict(),
+                        "crt2_net": td3.crt2_net.state_dict(),
+                        "update_cnt": td3.update_cnt,
+                        "frame_idx": frame_idx,
+                        "best_reward": best_reward,
+                        "target_crt1_net": td3.target_crt1_net.target_model.state_dict(),
+                        "target_crt2_net": td3.target_crt2_net.target_model.state_dict(),
+                        "target_act_net": td3.target_act_net.target_model.state_dict()
+                    }
+                    torch.save(checkpoint, os.path.join(save_path, "checkpoint_%d.dat" % (frame_idx % 10)))
                     if best_reward is None or best_reward < rewards:
                         if best_reward is not None:
                             print("Best reward updated: %.3f -> %.3f" % (best_reward, rewards))
                             name = "best_%+.3f_%d.dat" % (rewards, frame_idx)
                             fname = os.path.join(save_path, name)
-                            torch.save(act_net.state_dict(), fname)
-                        best_reward = rewards
 
+                            torch.save(checkpoint, fname)
+                        best_reward = rewards
     pass
